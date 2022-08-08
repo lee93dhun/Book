1a653cef6ea8d7aadc26aaccdf84132b2cba151b
lee93dhun <lee93dhun@gmail.com>
2022-08-08 14:50:09 +0900

    PostsApiController Test create

---
+++ src/main/java/com/megaIT/book/springboot/service/PostsService.java
@@ -0,0 +1,18 @@
+package com.megaIT.book.springboot.service;
+
+import com.megaIT.book.springboot.domain.posts.PostsRepository;
+import com.megaIT.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
