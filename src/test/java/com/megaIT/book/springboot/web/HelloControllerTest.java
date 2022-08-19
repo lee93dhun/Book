b79a9495243417ad0491e5943df4a20078d72991
lee93dhun <lee93dhun@gmail.com>
2022-08-19 15:27:41 +0900

    0819 test error catch

--- src/test/java/com/megaIT/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/megaIT/book/springboot/web/HelloControllerTest.java
@@ -1,13 +1,20 @@
 package com.megaIT.book.springboot.web;
 
+import com.megaIT.book.springboot.config.auth.SecurityConfig;
 import junit.framework.TestCase;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -15,23 +22,29 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 public class HelloControllerTest extends TestCase {
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloReturn() throws Exception{
         String hello = "hello";
 
-        mvc.perform(get("/hello"))
-                .andExpect(status().isOk())
-                .andExpect(content().string(hello));
+        mvc.perform(MockMvcRequestBuilders.get("/hello"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(hello));
 
-
+//        mvc.perform(get("/hello"))
+//                .andExpect(status().isOk())
+//                .andExpect(content().string(hello));
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDtoReturn() throws Exception{
         String name = "hello";
         int amount = 1000;
